fc5d3fb0218ce237677048a39ecb3a5bb11cbd62
Felty <leonelpineiro@gmail.com>
2023-11-17 11:43:46 -0300

    feat: UI welcome activity

---
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_pieiro_linco/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.parcial_1_am_acn4bv_pieiro_linco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
