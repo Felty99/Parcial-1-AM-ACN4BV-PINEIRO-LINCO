3febe086cc3c2e434c16e3979486c333208257e1
Felty <leonelpineiro@gmail.com>
2023-11-17 13:18:49 -0300

    feat: UI Answers

---
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_pieiro_linco/AnswersActivty.java
@@ -0,0 +1,14 @@
+package com.example.parcial_1_am_acn4bv_pieiro_linco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnswersActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answers_activty);
+    }
+}
