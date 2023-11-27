7e2fd95c4b5fcec023bb9a93cf0141134e691e8f
Felty <leonelpineiro@gmail.com>
2023-11-27 20:41:23 -0300

    add: lógica welcome activity

--- app/src/main/java/com/example/parcial_1_am_acn4bv_pieiro_linco/WelcomeActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_pieiro_linco/WelcomeActivity.java
@@ -2,13 +2,67 @@ package com.example.parcial_1_am_acn4bv_pieiro_linco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
+    TextView tvTitulo;
+    EditText etNombre;
+    Button btContinuar,btSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        etNombre=findViewById(R.id.etNombre);
+        btContinuar=findViewById(R.id.btContinuar);
+        btSalir = findViewById(R.id.btSalir);
+        btSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        etNombre.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 0) {
+                    btContinuar.setVisibility(View.VISIBLE);
+                } else {
+                    btContinuar.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        btContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = etNombre.getText().toString();
+
+                Intent continuar = new Intent(WelcomeActivity.this, RulesActivity.class);
+
+
+                continuar.putExtra("nombre",nombre);
+
+                startActivity(continuar);
+                finish();
+            }
+        });
     }
 }
