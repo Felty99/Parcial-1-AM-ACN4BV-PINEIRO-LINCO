09e9c61ca75146c6f50565a09535817760aa63db
Felty <leonelpineiro@gmail.com>
2023-12-16 14:18:06 -0300

    feat: traer mail y contraseña al login

--- app/src/main/java/com/example/parcial_1_am_acn4bv_pieiro_linco/LoginActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_pieiro_linco/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.parcial_1_am_acn4bv_pieiro_linco;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -10,9 +12,32 @@ public class LoginActivity extends AppCompatActivity {
     EditText etEmail, etPwd;
     Button btLogin;
 
+    public void login (String email, String password){
+        Log.i("firebase", "mail: "+ email);
+        Log.i("firebase", "password: "+ password);
+
+    }
+
+    public void onLoginButtonClick (View view){
+        EditText emailInput = findViewById(R.id.etEmail);
+        EditText passInput = findViewById(R.id.etPwd);
+
+        String email = emailInput.getText().toString();
+        String password = passInput.getText().toString();
+
+        this.login(email,password);
+    }
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btLogin=findViewById(R.id.btLogin);
+
+
+
     }
 }
