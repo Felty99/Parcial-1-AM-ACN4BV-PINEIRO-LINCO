cfc9a81e209e8d421648ca68aa2dd2bbb348c5e2
Felty <leonelpineiro@gmail.com>
2023-12-18 00:30:12 -0300

    feat: add strings

--- app/src/main/java/com/example/parcial_1_am_acn4bv_pieiro_linco/GameActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_pieiro_linco/GameActivity.java
@@ -89,6 +89,7 @@ public class GameActivity extends AppCompatActivity {
         LinearLayout Juego = findViewById(R.id.Juego);
         Juego.addView(respuestas);
         respuestas.setTextColor(+R.color.teal_700);
+        respuestas.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
         CompoundButton.OnCheckedChangeListener radioButtonsListener = new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
